bea1eae0a003bde32301ebec53cda52311ae05c3
Kenso Trabing <kenso.trabing@bloomwebsite.com>
2017-10-10 18:45:08 -0400

    Connect UI to IPFS and MediaChain

--- app.js
+++ app.js
@@ -10,45 +10,46 @@ const {BrowserWindow, Tray} = electron
 
 // Wait until the app is ready
 electron.app.once('ready', function () {
-  // Create a new window
-  window = new electron.BrowserWindow({
-    // Set the initial width to 500px
-    width: 1000,
-    // Set the initial height to 400px
-    height: 500,
-    // skipTaskbar: true,
-    // Show the minimize/maximize buttons inset in the window on macOS
-    // titleBarStyle: 'hidden-inset',
-    // Set the default background color of the window to match the CSS
-    // background color of the page, this prevents any white flickering
-    // backgroundColor: "#111",
-    // Don't show the window until it ready, this prevents any white flickering
-    // show: false
-    icon: __dirname + '/static/img/fav.png'
-  });
+    // Create a new window
+    window = new electron.BrowserWindow({
+        // Set the initial width to 500px
+        width: 1000,
+        // Set the initial height to 400px
+        height: 500,
+        // skipTaskbar: true,
+        // Show the minimize/maximize buttons inset in the window on macOS
+        // titleBarStyle: 'hidden-inset',
+        // Set the default background color of the window to match the CSS
+        // background color of the page, this prevents any white flickering
+        // backgroundColor: "#111",
+        // Don't show the window until it ready, this prevents any white flickering
+        // show: false
+        frame: false,
+        icon: __dirname + '/static/img/fav.png'
+    });
 
-  const tray = new Tray(__dirname + '/static/img/fav.png')
+    const tray = new Tray(__dirname + '/static/img/fav.png')
 
-  window.tray = tray;
+    window.tray = tray;
 
-  tray.on('click', () => {
-    window.isVisible() ? window.hide() : window.show()
-  })
-  window.on('show', () => {
-    tray.setHighlightMode('always')
-  })
-  window.on('hide', () => {
-    tray.setHighlightMode('never')
-  })
-  // Load a URL in the window to the local index.html path
-  window.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }));
+    tray.on('click', () => {
+        window.isVisible() ? window.hide() : window.show()
+    });
+    window.on('show', () => {
+        tray.setHighlightMode('always')
+    });
+    window.on('hide', () => {
+        tray.setHighlightMode('never')
+    });
+    // Load a URL in the window to the local index.html path
+    window.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
 
-  // Show window when page is ready
-  window.once('ready-to-show', function () {
-    window.show();
-  });
+    // Show window when page is ready
+    window.once('ready-to-show', function () {
+        window.show();
+    });
 });
