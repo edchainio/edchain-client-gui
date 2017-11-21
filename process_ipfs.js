e34da3389df4c8ea12f9d84896bffc6e8fedf699
VincentGitHub15 <vjchiodo@verizon.net>
2017-11-21 18:38:09 -0500

    darwin for OS

--- process_ipfs.js
+++ process_ipfs.js
@@ -7,8 +7,10 @@ var log = require('electron-log');
 
 var startIpfs = function(){
 	
-	const ipfsPath = path.resolve(__dirname,'./','bin','linux','ipfs daemon');
-	
+	// const ipfsPath = path.resolve(__dirname,'./','bin','linux','ipfs daemon');
+	const ipfsPath = path.resolve(__dirname,'./','bin','darwin','ipfs daemon');
+	process.stdout.write("terminal");
+
 	return exec(ipfsPath, function (err,stdout,stderr){
 	
 		pubsub.publish('uiLogging', {info: stdout});
