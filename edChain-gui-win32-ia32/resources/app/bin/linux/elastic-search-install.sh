4ece1d8e6a009abc41342ff6b71c3ae573f12806
harishrongala <harishrongala@gmail.com>
2018-03-09 17:21:42 -0500

    Debian Linux installer created

--- edChain-gui-win32-ia32/resources/app/bin/linux/elastic-search-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#install and start elastic search
-cd ../../
-mkdir elastic-search
-cd elastic-search
-curl -L -O https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.2.0.tar.gz
-tar -xvf elasticsearch-6.2.0.tar.gz
-cd elasticsearch-6.2.0/bin
-./elasticsearch
